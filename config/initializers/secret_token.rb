7e37cef5b794cae1b0f01629877d1ba9f4fdfe08
tknemuru <kojikoji.max.5252@gmail.com>
2013-09-24 19:42:34 -0700

    first commit
    RSpecの最初のコードを書き終えた

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sinope::Application.config.secret_key_base = '000abda34a3991025b33c54aad1d3d555760fe3c736db36601ced1860708ecb2591aa71ddb9b6e085c55ee3bdee1484ab4e3940e37624cae6cba7e41914345ac'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Sinope::Application.routes.draw do
+  root to: 'top#index'
+end
